6a08e10deba1f75cec2dddd45ac526f237254266
Ross Wickman <ross@rwick.it>
2021-03-01 16:23:43 -0600

    initial test

---
+++ src/Deploy-MCMStack.ps1
@@ -0,0 +1,54 @@
+Param(
+    [object[]] $Customer
+)
+
+$config = (Get-Content -Raw "config.json") -join "`n" | convertfrom-json
+
+function Deploy-MCMStack {
+    Import-Module AWSPowerShell.NetCore
+    if(Get-MCMStack){
+        Write-Host -ForegroundColor Blue $("INFO : {0} : Stack {1} already deployed. Will attempt to update stack." -f $Customer.Name, $config.Service.StackName)
+        Update-MCMStack
+    } else {
+        try {
+            New-CFNStack `
+                -StackName $config.Service.StackName `
+                -Description $config.Service.Description `
+                -Region $config.Service.Region `
+                -Capability 'CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND' `
+                -TemplateURL $config.Service.TemplateUrl `
+                -Credential $AccountCred
+            Write-Host -ForegroundColor Blue $("INFO : {0} : Deploying Stack {1}." -f $Customer.Name, $config.Service.StackName)
+        } catch {
+            Write-Host -ForegroundColor Red $("ERROR : {0} : {1}." -f $Customer.Name, $_.Exception.Message)
+        }
+    }
+}
+
+function Update-MCMStack {
+    try {
+        Update-CFNStack `
+            -StackName $config.Service.StackName `
+            -Description $config.Service.Description `
+            -Region $config.Service.Region `
+            -Capability 'CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND' `
+            -TemplateURL $config.Service.TemplateUrl `
+            -Credential $AccountCred
+        Write-Host -ForegroundColor Blue $("INFO : {0} : Updating Stack instances for {1}." -f $Customer.Name, $config.Service.StackName)
+    } catch {
+        Write-Host -ForegroundColor Red $("ERROR : {0} : {1}." -f $Customer.Name, $_.Exception.Message)
+    }
+}
+
+function Get-MCMStack {
+    try {
+        $stackDetails = (Get-CFNStack -StackName $config.Service.StackName -Region $config.Service.Region -Credential $AccountCred)
+    } catch {
+        Write-Host -ForegroundColor Blue $("INFO : {0} : {1}" -f $Customer.Name, $_.Exception.Message)
+    }
+    return $stackDetails
+ }
+
+$AccountCred = ./Get-MCMCustomerCredential
+
+Deploy-MCMStack
---
+++ src/Start-MCMStackOperations.ps1
@@ -0,0 +1,64 @@
+Import-Module AWSPowerShell.NetCore
+
+$config = (Get-Content -Raw config.json) -join "`n" | convertfrom-json
+
+function Start-MCMStackOperations {
+    if($config.Customers.length -eq 0){
+        Write-Host -ForegroundColor Magenta $("INFO : No customer accounts configured for this Stack.")
+    } else {
+        $deploy = @()
+        $update = @()
+        $remove = @()
+        foreach ($customer in $config.Customers) {
+            Switch (($customer.Status).ToLower()) {
+                "deploy" { $deploy += $customer }
+                "update" { $update += $customer }
+                "remove" { $remove += $customer }
+            }
+        }
+    }
+
+    if($deploy){
+        $deploy | ForEach-Object -Parallel {
+            $function:MCMDeployStack = $using:funcDeployDef
+            MCMDeployStack $_
+        }
+    }
+
+    if($update){
+        $update | ForEach-Object -Parallel {
+            $function:MCMDeployStack = $using:funcDeployDef
+            MCMDeployStack $_
+        }
+    }
+
+    if($remove){
+        $remove | ForEach-Object -Parallel {
+            $function:MCMRemoveStack = $using:funcRemoveDef
+            MCMRemoveStack $_
+        }
+    }
+}
+
+function MCMRemoveStack {
+    param(
+        [Parameter(Mandatory=$true,Position=0)]
+        [object[]] $customer
+    )
+    Write-Host -ForegroundColor Blue $("INFO : {0} : Creating [ REMOVE ] Job." -f $customer.Name)
+    .\Remove-MCMStack.ps1 -Customer $customer
+}
+$funcRemoveDef = $function:MCMRemoveStack.ToString() ## Get the function's definition *as a string* for paralle job
+
+function MCMDeployStack {
+    param(
+        [Parameter(Mandatory=$true,Position=0)]
+        [object[]] $customer
+    )
+
+    Write-Host -ForegroundColor Blue $("INFO : {0} : Creating [ DEPLOY ] Job." -f $customer.Name)
+    .\Deploy-MCMStack.ps1 -Customer $customer
+}
+$funcDeployDef = $function:MCMDeployStack.ToString() ## Get the function's definition *as a string* for paralle job
+
+Start-MCMStackOperations
